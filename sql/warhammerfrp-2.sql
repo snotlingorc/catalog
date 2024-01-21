f9c2bed808267d1cfd10486669701d672b7b7dca
Snotling <snotling@gmail.com>
2024-01-21 10:37:16 -0700

    cleaning the warhammer sql files

---
+++ sql/warhammerfrp-2.sql
@@ -0,0 +1,32 @@
+--
+-- Database: `catalog`
+-- Warhammer Fantasy Role Play books for 2nd, 4th edition
+-- --------------------------------------------------------
+--
+-- Author additions
+INSERT INTO `authors` (`id`, `Name`, `URL`, `creationDate`, `UpdateDate`) VALUES
+(6, 'Black Industries', 'https://en.wikipedia.org/wiki/Black_Industries', '2023-07-22 12:00:00', '2023-07-22 12:00:00'),
+(7, 'Fantasy Flight Games','https://www.fantasyflightgames.com/en/index/', '2023-07-22 12:00:00', '2023-07-22 12:00:00'),
+(8, 'Cubicle 7', 'https://cubicle7games.com/', '2023-07-22 12:00:00', '2023-07-22 12:00:00');
+--
+-- TAG additions
+INSERT INTO `tags` (`id`, `Name`, `CreationDate`, `UpdateDate`) VALUES
+(11, 'Warhammer FRP 2nd edition', '2023-07-22 12:00:00', '2023-07-22 12:00:00'),
+(12, 'Warhammer FRP 3rd edition', '2023-07-22 12:00:00', '2023-07-22 12:00:00'),
+(13, 'Warhammer FRP 4th edition', '2023-07-22 12:00:00', '2023-07-22 12:00:00');
+
+--  Stuff Addition from Black Industires, 2nd edition of whfrp
+INSERT INTO `stuff` (`id`, `Title`, `CatId`, `AuthorId`, `ConditionId`, `StatusId`, `OwnerId`, `Description`, `CreationDate`, `UpdateDate`) VALUES
+(3, 'Apocrypha Now', 1, 6, 1, 1, 1, 'additional rules, 1995', '2023-07-22 12:00:00', '2023-07-22 12:00:00'),
+
+--  Stuff Addition from Fantasy Flight Games, 2nd edition of whfrp
+INSERT INTO `stuff` (`id`, `Title`, `CatId`, `AuthorId`, `ConditionId`, `StatusId`, `OwnerId`, `Description`, `CreationDate`, `UpdateDate`) VALUES
+(3, 'Apocrypha Now', 1, 6, 1, 1, 1, 'additional rules, 1995', '2023-07-22 12:00:00', '2023-07-22 12:00:00'),
+
+--  Stuff Addition from Cubicle 7, 4th edition of whfrp
+INSERT INTO `stuff` (`id`, `Title`, `CatId`, `AuthorId`, `ConditionId`, `StatusId`, `OwnerId`, `Description`, `CreationDate`, `UpdateDate`) VALUES
+(3, 'Apocrypha Now', 1, 6, 1, 1, 1, 'additional rules, 1995', '2023-07-22 12:00:00', '2023-07-22 12:00:00'),
+
+
+-- Images
+--  INSERT INTO `image` (`id`, `stuffid`, `imageType`, `imageData`) VALUES
